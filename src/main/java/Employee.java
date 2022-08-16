d31662e7652d42e3181a7f3817a8571e256aa931
rbrynkus <renata.brynkus@gmail.com>
2022-08-16 08:55:20 +0200

    Removed unused methods.

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -12,28 +12,8 @@ public class Employee {
         this.employeeSalary = employeeSalary;
     }
 
-    public String getEmployeeFirstName() {
-        return employeeFirstName;
-    }
-
-    public void setEmployeeFirstName(String employeeFirstName) {
-        this.employeeFirstName = employeeFirstName;
-    }
-
-    public String getEmployeeLastName() {
-        return employeeLastName;
-    }
-
-    public void setEmployeeLastName(String employeeLastName) {
-        this.employeeLastName = employeeLastName;
-    }
-
     public double getEmployeeSalary() {
         return employeeSalary;
-    }
-
-    public void setEmployeeSalary(double employeeSalary) {
-        this.employeeSalary = employeeSalary;
     }
 
     public static double addAllSalaries(ArrayList<Employee> employeesList) {
